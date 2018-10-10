ac58cd1ad8c832f9e36e48bca017b0f7d1d44df9
Case <38591271+saladshootrdlux@users.noreply.github.com>
2018-10-09 19:02:15 -0700

    updated some pseudo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// Variables for tracking our wins, losses and ties. They begin at 0.
+// Variables for tracking our wins and losses. They begin at 0.
 
 var wins = 0;
 var losses = 0;
