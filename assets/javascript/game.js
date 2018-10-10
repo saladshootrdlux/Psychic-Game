ffe74321fe70b718bb522e5adbbb589d6ccb325a
Case <38591271+saladshootrdlux@users.noreply.github.com>
2018-10-09 19:01:28 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,6 @@
 
+// Sets the array of letter choices.
+
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
@@ -6,11 +8,8 @@ var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var guessedLetters = [];
 
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-console.log(computerChoice);
+// Reset function.
 
 function reset() {
     guessesLeft = 10;
@@ -18,6 +17,9 @@ function reset() {
     computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log("Letter to guess: " + computerChoice);
   }
+
+
+reset()
 
 // When the user presses a key, it will run the following function...
 
@@ -29,9 +31,6 @@ document.onkeypress = function (event) {
         guessedLetters.push(userGuess);
     }
     addUserGuess()
-    // var charCode = evt.keyCode || evt.which;
-    // OLD CODE FOR GENERATING LETTERS GUESSED //
-    // var lettersGuessed = String.fromCharCode(charCode);
 
 
     if (userGuess === computerChoice) {
@@ -53,19 +52,5 @@ document.onkeypress = function (event) {
     document.getElementById('losses').innerHTML = "Losses: " + losses;
     document.getElementById('guessesLeft').innerHTML = "Guesses Left: " + guessesLeft;
     document.getElementById('lettersGuessed').innerHTML = "Letters Guessed: " + guessedLetters;
-
-    // OLD CODE FOR GENERATING LETTERS GUESSED //
-    // document.getElementById("lettersGuessed").innerHTML += lettersGuessed;
-    // THIS DID NOT WORK AS INTENDED, UNABLE TO RESET //
-
-
-    // Create global empty array, e.g. "user guesses", with a rule if user's guess = computer's guess, add a win.
-    // Activate new game function and alert. Else, push user's guess into user guesses array. That way all user's guesses are pushed into array.//#endregion
-    // Then pulling doc.getelementid .innerHTML= Guesses so far: + userGuesses
-
-
-    // TWO BIG QUESTIONS
-    // 1. How do I reset the computer guess whenever I want to?
-    // 2. Is there a good way to use "String.fromCharCode(charCode)" to achieve the goal outlined for this game? Should I be using array?
 
 }
